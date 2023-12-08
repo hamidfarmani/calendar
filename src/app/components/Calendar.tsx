d1bf60bae5daa77b1885892c1c58a3a65954b503
Hamid Farmani <hamidfarmani1@gmail.com>
2023-12-08 10:48:36 +0100

    feat: setup project scaffolding
    - add mui
    - add nager public holidays
    - show red dot on holidays

---
+++ src/app/components/Calendar.tsx
@@ -0,0 +1,129 @@
+"use client";
+import React from "react";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import dayjs, { Dayjs } from "dayjs";
+import {
+  DayCalendarSkeleton,
+  LocalizationProvider,
+  PickersDay,
+  PickersDayProps,
+} from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Badge } from "@mui/material";
+import axios from "axios";
+
+interface HolidayData {
+  date: string;
+  localName: string;
+  name: string;
+  countryCode: string;
+  fixed: boolean;
+  global: boolean;
+  counties: string[] | null;
+  launchYear: number | null;
+  types: string[];
+}
+
+async function fetchHolidays(date: Dayjs) {
+  const year = date.year();
+  const month = date.month() + 1;
+  const countryCode = "SE";
+
+  try {
+    const response = await axios.get<HolidayData[]>(
+      `https://date.nager.at/api/v3/PublicHolidays/${year}/${countryCode}`
+    );
+
+    const daysToHighlight = response.data
+      .filter((holiday) => dayjs(holiday.date).month() + 1 === month)
+      .map((holiday) => dayjs(holiday.date).date());
+
+    console.log(response);
+    return { daysToHighlight };
+  } catch (error) {
+    throw error;
+  }
+}
+
+const initialValue = dayjs();
+
+function ServerDay(
+  props: PickersDayProps<Dayjs> & { highlightedDays?: number[] }
+) {
+  const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
+
+  const isSelected =
+    !props.outsideCurrentMonth &&
+    highlightedDays.indexOf(props.day.date()) >= 0;
+
+  return (
+    <Badge
+      key={props.day.toString()}
+      overlap="circular"
+      badgeContent={isSelected ? "🔴" : undefined}
+    >
+      <PickersDay
+        {...other}
+        outsideCurrentMonth={outsideCurrentMonth}
+        day={day}
+      />
+    </Badge>
+  );
+}
+
+export default function DateCalendarServerRequest() {
+  const requestAbortController = React.useRef<AbortController | null>(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [highlightedDays, setHighlightedDays] = React.useState([1, 2, 15]);
+
+  const fetchHighlightedDays = (date: Dayjs) => {
+    const controller = new AbortController();
+    fetchHolidays(date)
+      .then(({ daysToHighlight }) => {
+        setHighlightedDays(daysToHighlight);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        if (error.name !== "AbortError") {
+          throw error;
+        }
+      });
+
+    requestAbortController.current = controller;
+  };
+
+  React.useEffect(() => {
+    fetchHighlightedDays(initialValue);
+    return () => requestAbortController.current?.abort();
+  }, []);
+
+  const handleMonthChange = (date: Dayjs) => {
+    if (requestAbortController.current) {
+      requestAbortController.current.abort();
+    }
+
+    setIsLoading(true);
+    setHighlightedDays([]);
+    fetchHighlightedDays(date);
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar
+        displayWeekNumber
+        defaultValue={initialValue}
+        loading={isLoading}
+        onMonthChange={handleMonthChange}
+        renderLoading={() => <DayCalendarSkeleton />}
+        slots={{
+          day: ServerDay,
+        }}
+        slotProps={{
+          day: {
+            highlightedDays,
+          } as any,
+        }}
+      />
+    </LocalizationProvider>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import DateCalendarServerRequest from "./components/Calendar";
+
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: "gray" }}>
+      <DateCalendarServerRequest />
+    </div>
+  );
+}
